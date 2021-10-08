3af021720056f89c71fe82169f9e58e834817928
AM-ops <33016763@g.nwu.ac.za>
2021-10-08 17:41:21 +0200

    Add files via upload

---
+++ efundi.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+#printf "Starting sync process for Data Warehouses - ITRI611!\n"
+#rclone sync ITRI611:/ ~/Documents/ITRI611/ > /dev/null 2>&1
+#printf "Done sync process for Data Warehouses - ITRI611!\n\n"
+
+#printf "Starting sync process for Databases - ITRI613!\n"
+#rclone sync ITRI613:/ ~/Documents/ITRI613/ > /dev/null 2>&1
+#printf "Done sync process for Databases - ITRI613!\n\n"
+
+#printf "Starting sync process for Computer Security - ITRI615!\n"
+#rclone sync ITRI615:/ ~/Documents/ITRI615/ > /dev/null 2>&1
+#printf "Done sync process for Computer Security - ITRI615!\n\n"
+
+#printf "Starting sync process for Artificial Intelligence - ITRI616!\n"
+#rclone sync ITRI616:/ ~/Documents/ITRI616/ > /dev/null 2>&1
+#printf "Done sync process for Artificial Intelligence - ITRI616!\n\n"
+
+#printf "Starting sync process for Project Module - ITRI671!\n"
+#rclone sync ITRI671:/ ~/Documents/ITRI671/ > /dev/null 2>&1
+#printf "Done sync process for Project Module - ITRI671!\n\n"
+
+#printf "Starting sync process for Ethical Perspectives - PHIL213!\n"
+#rclone sync PHIL213:/ ~/Documents/PHIL213/ > /dev/null 2>&1
+#printf "Done sync process for Ethical Perspectives - PHIL213!\n\n"
+
+printf "Starting sync process for Data Warehousing II - ITRI621!\n"
+rclone sync ITRI621:/ ~/Documents/ITRI621/ > /dev/null 2>&1
+printf "Done sync process for Data Warehousing II - ITRI621!\n\n"
+
+printf "Starting sync process for Databases II - ITRI623!\n"
+rclone sync ITRI623:/ ~/Documents/ITRI623/ > /dev/null 2>&1
+printf "Done sync process for Databases II - ITRI623!\n\n"
+
+printf "Starting sync process for Computer Security II - ITRI625!\n"
+rclone sync ITRI625:/ ~/Documents/ITRI625/ > /dev/null 2>&1
+printf "Done sync process for Computer Security - ITRI625!\n\n"
+
+printf "Starting sync process for Artificial Intelligence II - ITRI626!\n"
+rclone sync ITRI626:/ ~/Documents/ITRI626/ > /dev/null 2>&1
+printf "Done sync process for Artificial Intelligence II - ITRI626!\n\n"
+
+printf "Starting sync process for Tutors 2021!\n"
+rclone sync Tutors:/ ~/Documents/Tutors/ > /dev/null 2>&1
+printf "Done sync process for Tutors!\n\n"
+
+printf "Starting sync process for Advanced Databases - CMPG321!\n"
+rclone sync CMPG321:/ ~/Documents/CMPG321/ > /dev/null 2>&1
+printf "Done sync process for Advanced Databases - CMPG321!\n\n"
