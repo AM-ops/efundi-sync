73505fee348bd0b70a30b6533d8d8c0dee387f08
AM-ops <33016763@g.nwu.ac.za>
2021-10-08 17:42:08 +0200

    Update efundi.sh

--- efundi.sh
+++ efundi.sh
@@ -1,27 +1,27 @@
 #!/bin/bash
-#printf "Starting sync process for Data Warehouses - ITRI611!\n"
-#rclone sync ITRI611:/ ~/Documents/ITRI611/ > /dev/null 2>&1
-#printf "Done sync process for Data Warehouses - ITRI611!\n\n"
+printf "Starting sync process for Data Warehouses - ITRI611!\n"
+rclone sync ITRI611:/ ~/Documents/ITRI611/ > /dev/null 2>&1
+printf "Done sync process for Data Warehouses - ITRI611!\n\n"
 
-#printf "Starting sync process for Databases - ITRI613!\n"
-#rclone sync ITRI613:/ ~/Documents/ITRI613/ > /dev/null 2>&1
-#printf "Done sync process for Databases - ITRI613!\n\n"
+printf "Starting sync process for Databases - ITRI613!\n"
+rclone sync ITRI613:/ ~/Documents/ITRI613/ > /dev/null 2>&1
+printf "Done sync process for Databases - ITRI613!\n\n"
 
-#printf "Starting sync process for Computer Security - ITRI615!\n"
-#rclone sync ITRI615:/ ~/Documents/ITRI615/ > /dev/null 2>&1
-#printf "Done sync process for Computer Security - ITRI615!\n\n"
+printf "Starting sync process for Computer Security - ITRI615!\n"
+rclone sync ITRI615:/ ~/Documents/ITRI615/ > /dev/null 2>&1
+printf "Done sync process for Computer Security - ITRI615!\n\n"
 
-#printf "Starting sync process for Artificial Intelligence - ITRI616!\n"
-#rclone sync ITRI616:/ ~/Documents/ITRI616/ > /dev/null 2>&1
-#printf "Done sync process for Artificial Intelligence - ITRI616!\n\n"
+printf "Starting sync process for Artificial Intelligence - ITRI616!\n"
+rclone sync ITRI616:/ ~/Documents/ITRI616/ > /dev/null 2>&1
+printf "Done sync process for Artificial Intelligence - ITRI616!\n\n"
 
-#printf "Starting sync process for Project Module - ITRI671!\n"
-#rclone sync ITRI671:/ ~/Documents/ITRI671/ > /dev/null 2>&1
-#printf "Done sync process for Project Module - ITRI671!\n\n"
+printf "Starting sync process for Project Module - ITRI671!\n"
+rclone sync ITRI671:/ ~/Documents/ITRI671/ > /dev/null 2>&1
+printf "Done sync process for Project Module - ITRI671!\n\n"
 
-#printf "Starting sync process for Ethical Perspectives - PHIL213!\n"
-#rclone sync PHIL213:/ ~/Documents/PHIL213/ > /dev/null 2>&1
-#printf "Done sync process for Ethical Perspectives - PHIL213!\n\n"
+printf "Starting sync process for Ethical Perspectives - PHIL213!\n"
+rclone sync PHIL213:/ ~/Documents/PHIL213/ > /dev/null 2>&1
+printf "Done sync process for Ethical Perspectives - PHIL213!\n\n"
 
 printf "Starting sync process for Data Warehousing II - ITRI621!\n"
 rclone sync ITRI621:/ ~/Documents/ITRI621/ > /dev/null 2>&1
